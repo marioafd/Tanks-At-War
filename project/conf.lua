19215418e0762b456d1e991a94b28c573e824f54
Mario Silva <109557220+MarioAFD17@users.noreply.github.com>
2023-01-25 17:13:09 +0000

    Delete project directory

--- project/conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-    t.window.title = "Tanks_At_War"
-    t.window.width = 1280               
-    t.window.height = 720
-    t.window.fullscreen = false
-    t.window.resizable = false        
-end
